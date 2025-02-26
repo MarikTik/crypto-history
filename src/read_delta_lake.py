b3e2a5d800e580b9ddee16f86c46ab19e5398de7
Mark <mtik.philosopher@gmail.com>
2025-02-26 06:50:52 +0000

    removed example delta_lake file

--- src/read_delta_lake.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-delta_path = "delta"
-# Read the Delta table using DuckDB
-query = f"SELECT * FROM read_parquet('{delta_path}/*.parquet')"
-
-df = duckdb.query(query).to_df()
-
-print(df)
