e41d2be12fc16685d7c2c394a8b6702c9e8a5e25
Mark <mtik.philosopher@gmail.com>
2025-02-26 06:47:17 +0000

    updated main

--- src/main.py
+++ src/main.py
@@ -1,32 +1,9 @@
 import asyncio
 from pathlib import Path
-import pyarrow as pa
 from coin_db import CoinDB
 from parser import Parser
 from coinbase_candle_history import CoinbaseCandleHistory
-from parquet import Parquet
-import logging
 
-logging.basicConfig(filename='logs/log', level=logging.INFO, 
-                        format='%(message)s')
-# SCHEMA = pa.schema([
-#      ("time", pa.int64()),
-#      ("low", pa.float64()),
-#      ("high", pa.float64()),
-#      ("open", pa.float64()),
-#      ("close", pa.float64()),
-#      ("volume", pa.float64()),
-#      ("timestamp", pa.timestamp("s"))
-# ])
-
-# COLUMNS = ["time", "low", "high", "open", "close", "volume"]
-
-# SORT_KEY = "timestamp"
- 
-
-with Path("extras", "coin-pairs").open("r") as file:
-     coins = file.readlines()
-     
 async def main(symbols, start_date, end_date, granularity, dir):
      
      gen = CoinbaseCandleHistory.fetch(
