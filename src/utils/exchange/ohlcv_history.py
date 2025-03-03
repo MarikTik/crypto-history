52cf07d6f047eea9032546025a68716e7f969c5f
Mark <mtik.philosopher@gmail.com>
2025-03-03 08:06:17 +0000

    added static method fetch_many

--- src/utils/exchange/ohlcv_history.py
+++ src/utils/exchange/ohlcv_history.py
@@ -1,15 +1,65 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Union, Optional
+from typing import Union, Optional, Dict, TypeVar, Generic, Any, Type, AsyncGenerator
 
-class OHLCV_History(ABC):
-    def __init__(self, product: str):
+ 
+T = TypeVar("T", bound="OHLCV_History")
+
+class OHLCV_History(ABC, Generic[T]):
+    def __init__(self, exchange: str, product: str, granularity: int):
+        self._exchange = exchange
         self._product = product
-        
+        self._granularity = granularity
+
+    async def __aenter__(self):
+        """Async context manager entry: Create session."""
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """Async context manager exit: Close session properly."""
+        pass
+
     @abstractmethod
     async def fetch_timeframe(
         self,
         start_time: datetime,
         end_time: Optional[datetime],
-        granularity: int):
+        granularity: int) -> AsyncGenerator[Any, None]:
         pass
+    
+    @abstractmethod
+    async def fetch(
+        self,
+        start_date: Optional[Union[str, datetime]],
+        end_date: Optional[Union[str, datetime]],
+        granularity: int,
+        default_start_date: str) -> AsyncGenerator[Any, None]:
+        pass
+
+    @staticmethod
+    async def fetch_many(cls: Type[T], products: Dict[str, Dict]) -> AsyncGenerator[Any, None]:
+        """
+        Fetches OHLCV data for multiple products asynchronously.
+        This method allows selecting which subclass of OHLCV_History should be used.
+
+        Args:
+            cls (Type[T]): Subclass of OHLCV_History (e.g., CoinbaseOHLCV, BinanceOHLCV).
+            products (Dict[str, Dict]): Dictionary with products and fetch parameters:
+            {
+                "BTC-USD": {
+                    "start_date": datetime | str | None,  # If None, starts from the earliest record
+                    "end_date": datetime | str | None,  # If None, fetches until now
+                    "granularity": int | None  # If None, defaults to the subclass's default granularity
+                },
+                "ETH-USD": { ... }, ...
+            }
+         Yields:
+            Dict[str, Any]: The fetched OHLCV data per product (untill finished).
+        """
+
+        for product, params in products.items():
+            start_date, end_date, granularity = [params.get(key, None) for key in ["start_date", "end_date", "granularity"]]
+            async with cls(product, granularity) as instance:   
+                async for data in instance.fetch(start_date, end_date, granularity):
+                    yield {product : data}   
+       
