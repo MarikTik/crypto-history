81d0c2ec6a555ddaf83c1e79d5b3aecb94d3e154
Mark <mtik.philosopher@gmail.com>
2025-03-03 20:02:19 +0000

    updated order book interface

--- src/utils/exchange/order_book.py
+++ src/utils/exchange/order_book.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import AsyncGenerator, Optional, Union, Dict
+from typing import AsyncGenerator, Optional, Union, Dict, List
 from datetime import datetime
 
 class OrderBook(ABC):
-    def __init__(self, product: str, granularity: float | int, depth: int):
+    def __init__(self, products: List[str], frequency: float | int, depth: int):
         """
         Base class for an order book.
         
@@ -12,10 +12,17 @@ class OrderBook(ABC):
             granularity (float): Time interval (seconds) between snapshots.
             depth (int): Number of order book levels to maintain.
         """
-        self._product = product
-        self._granularity = granularity
+        self._products = products
+        self._frequency = frequency
         self._depth = depth
-        
+
+    @abstractmethod    
+    async def __aenter__(self):
+        """Async context manager entry: Initializes WebSocket client."""
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """Async context manager exit: Closes WebSocket client."""
 
     @abstractmethod
     async def snapshots(untill: Optional[Union[str, datetime]]) -> AsyncGenerator[Dict, None]:
@@ -23,7 +30,7 @@ class OrderBook(ABC):
         Abstract generator for yielding order book snapshots.
 
         Args:
-            until (Optional[Union[str, datetime]]): Timestamp or ISO format string indicating when to stop collecting.
+            until (Optional[Union[str, datetime]]): Timestamp or ISO format string indicating when to stop collecting. If None given than it will not stop.
 
         Yields:
             dict: The current order book snapshot.
