83c0f41cde395b437653104f2ae4cf41a43cd84c
Mark <mtik.philosopher@gmail.com>
2025-03-03 07:05:20 +0000

    renamed to OHLCV_History

--- src/utils/exchange/__init__.py
+++ src/utils/exchange/__init__.py
@@ -1,3 +1,3 @@
 from .database import DataBase
-from .ohlcv_history import OHLCV_HISTRY
+from .ohlcv_history import OHLCV_History
 from .order_book import OrderBook
--- src/utils/exchange/ohlcv_history.py
+++ src/utils/exchange/ohlcv_history.py
@@ -1,3 +1,15 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Union, Optional
 
-class OHLCV_HISTRY:
-    pass
+class OHLCV_History(ABC):
+    def __init__(self, product: str):
+        self._product = product
+        
+    @abstractmethod
+    async def fetch_timeframe(
+        self,
+        start_time: datetime,
+        end_time: Optional[datetime],
+        granularity: int):
+        pass
