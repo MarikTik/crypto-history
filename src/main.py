c6946e934545af337bff4fad55f8e80c98d74741
Mark <mtik.philosopher@gmail.com>
2025-03-16 03:23:34 +0000

    modifed main to reflect current changes

--- src/main.py
+++ src/main.py
@@ -1,50 +1,42 @@
-# import asyncio
-# from pathlib import Path
-# from coin_db import CoinDB
-# from parser import Parser
-# from exchanges.coinbase import OHLCV_History
-# from datetime import datetime
-
-# from utils.exchange import *
-# import exchanges.coinbase as coinbase
-# import exchanges.binance as binance
-# import exchanges.kraken as kraken
-# import exchanges.robinhood as robinhood
-
-# exchanges = {exchange.__name__.split(".")[-1] : exchange for exchange in [coinbase, binance, kraken, robinhood]}
-
-# async def download_order_book(exchange, products, depth, frequency, end_date, directory):
-#      db: Database = exchange.Database(directory=directory)
-#      async with exchange.OrderBook(products=products.keys(), depth=depth, frequency=frequency) as order_book:
-#           async for snapshot in order_book.snapshots(until=end_date):
-#                await db.store_order_book_snapshot(snapshot)
-
-# async def download_ohlcv(exchange, products, directory):
-#      db: Database = exchange.Database(directory=directory)
-#      gen = exchange.OHLCV_History.fetch_many(exchange.OLHCV_History, products)
-#      async for candles in gen:
-#           await db.store_ohlcv_candles(candles)
-
-# async def main(*args):
-#      download_type, exchange, products, extras, directory = args
-#      if download_type == "ohlcv":
-#           await download_ohlcv(exchange, products, directory)
-#      elif download_type == "order_book":
-#           await download_order_book(exchange, products, *extras, directory)
-
-# if __name__ == "__main__":
-#      parser = Parser()
-#      args = parser.parse()
-#      asyncio.run(main(*args))
-
-from exchanges.coinbase import OHLCV_History
-from datetime import datetime
 import asyncio
-import json
-async def main():
-     async with OHLCV_History("BTC-USD", 60) as f:
-          async for batch in f.fetch():
-               print(batch)
-               print("\n\n")
+from pathlib import Path
+from parser import Parser
+from datetime import datetime
+from typing import Dict, List
+from utils.exchange import OHLCV_Database, OrderBookDatabase, Exchange
 
-asyncio.run(main())
+from exchanges import *
+
+exchanges: Dict[str, Exchange] = {
+     "binance": Binance,
+     "coinbase" : Coinbase,
+     "kraken" : Kraken,
+     "robinhood" : Robinhood
+     }
+
+async def download_order_book(exchange: Exchange, products: List[str], depth: int, frequency: float | int, end_date: datetime, directory: Path):
+     async with exchange.order_book(products=products, frequency=frequency, depth=depth) as order_book:
+          with OrderBookDatabase(directory) as db:
+               async for snapshot in order_book.snapshots(until=end_date):
+                    db.write(snapshot)
+
+async def download_ohlcv(exchange: Exchange, products: Dict[str, Dict], directory: Path):
+     gen = exchange.ohlcv.fetch_many(products=products)
+     with OHLCV_Database(directory) as db:
+          async for candles in gen:
+               db.write(candles)
+
+async def main(*args):
+     download_type, exchange_name, products, extras, directory = args
+     exchange = exchanges[exchange_name.lower()]
+     if download_type == "ohlcv":
+          await download_ohlcv(exchange, products, directory)
+     elif download_type == "order_book":
+          await download_order_book(exchange, products.keys(), *extras, directory)
+
+if __name__ == "__main__":
+     parser = Parser()
+     args = parser.parse()
+     asyncio.run(main(*args))
+
+ 
