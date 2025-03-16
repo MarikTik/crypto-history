a7cc99b07c062341373ad6e468ecd9234ff83e38
Mark <mtik.philosopher@gmail.com>
2025-03-16 01:52:33 +0000

    added exchange implemntation per each exchange used

--- src/exchanges/binance/__init__.py
+++ src/exchanges/binance/__init__.py
@@ -1,2 +1,3 @@
 from .ohlcv_history import OHLCV_History
 from .order_book import OrderBook
+from .binance import Binance
---
+++ src/exchanges/robinhood/robinhood.py
@@ -0,0 +1,7 @@
+from utils.exchange import Exchange
+from ohlcv_history import OHLCV_History
+from order_book import OrderBook
+
+class Robinhood(Exchange):
+    ohlcv = OHLCV_History
+    order_book = OrderBook
