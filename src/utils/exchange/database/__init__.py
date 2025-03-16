ee0599bd5f4677771994519c1e107be75aa212d3
Mark <mtik.philosopher@gmail.com>
2025-03-16 03:22:49 +0000

    fixed import issues

--- src/utils/exchange/database/__init__.py
+++ src/utils/exchange/database/__init__.py
@@ -1,3 +1,3 @@
 from .write_only_database import *
 from .ohlcv_database import *
-from .order_book import *
+from .order_book_database import *
--- src/utils/exchange/database/order_book_database.py
+++ src/utils/exchange/database/order_book_database.py
@@ -1,5 +1,28 @@
+from pathlib import Path
+from typing import Dict, Any
 from .write_only_database import WriteOnlyDatabase
 
 class OrderBookDatabase(WriteOnlyDatabase):
-    pass
+     
+    def __init__(self, directory: Path):
+        """
+        Initializes the database with a specified storage directory.
+
+        Args:
+            directory (Path): The root directory for storing OHLCV or order book data.
+                              If the directory does not exist, it will be created.
+        """
+        directory.mkdir(parents=True, exist_ok=True)
+        self._directory = directory
+
     
+    def write(self, product_records: Dict[str, Any]) -> None:
+        pass
+
+ 
+    def __enter__(self):
+        return self
+
+   
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
