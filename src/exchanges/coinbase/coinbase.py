12022957e6f915261e989c3e9a468fe79f228dd1
Mark <mtik.philosopher@gmail.com>
2025-03-30 06:00:35 +0000

    updated coinbase class to implement 'Exchange' logic

--- src/exchanges/coinbase/coinbase.py
+++ src/exchanges/coinbase/coinbase.py
@@ -1,6 +1,23 @@
+"""
+File: coinbase.py
+
+This module provides the Coinbase exchange implementation, extending the base
+Exchange class to handle Coinbase-specific API interactions for fetching
+trading product information.
+
+It defines the Coinbase class, which is responsible for:
+
+    Fetching lists of traded and non-traded products from the Coinbase API.
+    Handling potential API errors and network issues.
+    Scheduling updates for product information.
+
+The module also includes example usage for scheduling updates within a specific
+UTC timeframe.
+"""
+
 import aiohttp
-from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
+from datetime import time, timedelta, timezone
 
 from ..exchange import Exchange
 from .ohlcv_history import OHLCV_History
@@ -8,15 +25,28 @@ from .order_book import OrderBook
 
 
 class Coinbase(Exchange):
+    """
+    Coinbase Exchange implementation.
+
+    This class provides Coinbase-specific implementations for fetching trading products.
+    """
+
     ohlcv = OHLCV_History
     order_book = OrderBook
 
-    def __init__(
-        self, logs_path: Path = Path("logs", "coinbase", "exchange.log")
-    ):
-        super().__init__(logs_path)
+    @staticmethod
+    async def fetch_products() -> Tuple[set[str], set[str]]:
+        """
+        Fetches trading and non-trading products from the Coinbase API.
 
-    async def fetch_products(self):
+        Returns:
+            Tuple[Set[str], Set[str]]:
+                - The first set contains currently **traded** products.
+                - The second set contains **non-traded** products (including delisted ones).
+
+        Raises:
+            RuntimeError: If the API request fails or returns an invalid response.
+        """
         PRODUCT_URL = "https://api.exchange.coinbase.com/products"
         async with aiohttp.ClientSession() as session:
             try:
@@ -45,3 +75,9 @@ class Coinbase(Exchange):
             except Exception as e:
                 raise RuntimeError(f"Unexpected error: {e}")
         return trading_products, non_trading_products
+
+
+start_utc = time(18, 0, tzinfo=timezone.utc)
+end_utc = time(22, 30, tzinfo=timezone.utc)
+# scheduling updates with one minute granularity  on the interval 18:00 - 22:30 UTC
+Coinbase.schedule_updates([((start_utc, end_utc), timedelta(minutes=1))])
